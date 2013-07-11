e873de63d2bc641bafdde59ed3ab708aa5b615dc
Arnaud Héritier <aheritier@exoplatform.com>
2013-07-11 17:01:57 +0200

    Allow to remove a rule previously added. Just add parameter ensure => 'absent' in ufw::allow or ufw::deny definitions

--- manifests/allow.pp
+++ manifests/allow.pp
@@ -1,7 +1,7 @@
-define ufw::allow($proto='tcp', $port='all', $ip='', $from='any') {
+define ufw::allow($proto='tcp', $port='all', $ip='', $from='any', $ensure = 'present') {
 
-  $ipadr = $ip ? {
-    ''  => $::ipaddress_eth0 ? {
+  $ipadr      = $ip ? {
+    ''      => $::ipaddress_eth0 ? {
       undef   => 'any',
       default => $::ipaddress_eth0,
     },
@@ -13,18 +13,42 @@ define ufw::allow($proto='tcp', $port='all', $ip='', $from='any') {
     default => "$from",
   }
 
-  exec { "ufw-allow-${proto}-from-${from}-to-${ipadr}-port-${port}":
-    command => $port ? {
-      'all'   => "ufw allow proto $proto from $from to $ipadr",
-      default => "ufw allow proto $proto from $from to $ipadr port $port",
-    },
-    unless  => "$ipadr:$port" ? {
-      'any:all'    => "ufw status | grep -E \" +ALLOW +$from_match\"",
-      /[0-9]:all$/ => "ufw status | grep -E \"$ipadr/$proto +ALLOW +$from_match\"",
-      /^any:[0-9]/ => "ufw status | grep -E \"$port/$proto +ALLOW +$from_match\"",
-      default      => "ufw status | grep -E \"$ipadr $port/$proto +ALLOW +$from_match\"",
-    },
-    require => Exec['ufw-default-deny'],
-    before  => Exec['ufw-enable'],
+  case $ensure {
+    'present' : {
+      exec { "add-ufw-allow-${proto}-from-${from}-to-${ipadr}-port-${port}":
+        command => $port ? {
+          'all'   => "ufw allow proto $proto from $from to $ipadr",
+          default => "ufw allow proto $proto from $from to $ipadr port $port",
+        },
+        unless  => "$ipadr:$port" ? {
+          'any:all'    => "ufw status | grep -E \" +ALLOW +$from_match\"",
+          /[0-9]:all$/ => "ufw status | grep -E \"$ipadr/$proto +ALLOW +$from_match\"",
+          /^any:[0-9]/ => "ufw status | grep -E \"$port/$proto +ALLOW +$from_match\"",
+          default      => "ufw status | grep -E \"$ipadr $port/$proto +ALLOW +$from_match\"",
+        },
+        require => Exec['ufw-default-deny'],
+        before  => Exec['ufw-enable'],
+      }
+    }
+    'absent'  : {
+      exec { "delete-ufw-allow-${proto}-from-${from}-to-${ipadr}-port-${port}":
+        command => $port ? {
+          'all'   => "ufw delete allow proto $proto from $from to $ipadr",
+          default => "ufw delete allow proto $proto from $from to $ipadr port $port",
+        },
+        onlyif  => "$ipadr:$port" ? {
+          'any:all'    => "ufw status | grep -E \" +ALLOW +$from_match\"",
+          /[0-9]:all$/ => "ufw status | grep -E \"$ipadr/$proto +ALLOW +$from_match\"",
+          /^any:[0-9]/ => "ufw status | grep -E \"$port/$proto +ALLOW +$from_match\"",
+          default      => "ufw status | grep -E \"$ipadr $port/$proto +ALLOW +$from_match\"",
+        },
+        require => Exec['ufw-default-deny'],
+        before  => Exec['ufw-enable'],
+      }
+    }
+    default   : {
+      fail("Unrecognized ensure parameter. Must be 'present' or 'absent'.")
+    }
   }
+
 }
--- manifests/deny.pp
+++ manifests/deny.pp
@@ -1,4 +1,4 @@
-define ufw::deny($proto='tcp', $port='all', $ip='', $from='any') {
+define ufw::deny($proto='tcp', $port='all', $ip='', $from='any', $ensure = 'present') {
 
   if $::ipaddress_eth0 != undef {
     $ipadr = $ip ? {
@@ -14,16 +14,37 @@ define ufw::deny($proto='tcp', $port='all', $ip='', $from='any') {
     default => "$from",
   }
 
-  exec { "ufw-deny-${proto}-from-${from}-to-${ipadr}-port-${port}":
-    command => $port ? {
-      'all'   => "ufw deny proto $proto from $from to $ipadr",
-      default => "ufw deny proto $proto from $from to $ipadr port $port",
-    },
-    unless  => $port ? {
-      'all'   => "ufw status | grep -E \"$ipadr/$proto +DENY +$from_match\"",
-      default => "ufw status | grep -E \"$ipadr $port/$proto +DENY +$from_match\"",
-    },
-    require => Exec['ufw-default-deny'],
-    before  => Exec['ufw-enable'],
+  case $ensure {
+    'present' : {
+      exec { "add-ufw-deny-${proto}-from-${from}-to-${ipadr}-port-${port}":
+        command => $port ? {
+          'all'   => "ufw deny proto $proto from $from to $ipadr",
+          default => "ufw deny proto $proto from $from to $ipadr port $port",
+        },
+        unless  => $port ? {
+          'all'   => "ufw status | grep -E \"$ipadr/$proto +DENY +$from_match\"",
+          default => "ufw status | grep -E \"$ipadr $port/$proto +DENY +$from_match\"",
+        },
+        require => Exec['ufw-default-deny'],
+        before  => Exec['ufw-enable'],
+      }
+    }
+    'absent'  : {
+      exec { "delete-ufw-deny-${proto}-from-${from}-to-${ipadr}-port-${port}":
+        command => $port ? {
+          'all'   => "ufw delete deny proto $proto from $from to $ipadr",
+          default => "ufw delete deny proto $proto from $from to $ipadr port $port",
+        },
+        onlyif  => $port ? {
+          'all'   => "ufw status | grep -E \"$ipadr/$proto +DENY +$from_match\"",
+          default => "ufw status | grep -E \"$ipadr $port/$proto +DENY +$from_match\"",
+        },
+        require => Exec['ufw-default-deny'],
+        before  => Exec['ufw-enable'],
+      }
+    }
+    default   : {
+      fail("Unrecognized ensure parameter. Must be 'present' or 'absent'.")
+    }
   }
 }
